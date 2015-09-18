7ee9b8be1a6ac2ff30e2c1a9d92741341b36c882
Kevin Quick <kquick@godaddy.com>
2015-09-18 11:01:29 -0700

    Added the url and long description in the setup.py file.

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     description = 'Python Actor concurrency library',
     author = 'Kevin Quick',
     author_email = 'kquick@godaddy.com',
+    url = 'https://github.com/godaddy/Thespian',
     license = 'MIT',
     scripts = [ 'thespianShell.py' ],
     packages = find_packages(exclude=['thespian/test']),
@@ -17,5 +18,26 @@ setup(
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-    ]
+    ],
+    long_description = '''
+
+    Thespian is a Python library providing a framework for developing
+    concurrent, distributed, fault tolerant applications.
+
+    Thespian is built on the Actor Model which allows applications to
+    be written as a group of independently executing but cooperating
+    "Actors" which communicate via messages.  These Actors run within
+    the Actor System provided by the Thespian library.
+
+      * Concurrent
+      * Distributed
+      * Fault Tolerant
+      * Scalable
+      * Location independent
+
+    Actor programming is broadly applicable and it is ideally suited
+    for Cloud-based applications as well, where compute nodes are
+    added and removed from the environment dynamically.
+
+  '''
 )
