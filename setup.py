96e97cd321510d690b3be099026bd11cb7c4a60c
Kevin Quick <kquick@godaddy.com>
2015-09-27 22:47:55 -0700

    Update to version 2.1.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'thespian',
-    version = '2.1.5',
+    version = '2.1.6',
     description = 'Python Actor concurrency library',
     author = 'Kevin Quick, GoDaddy',
     author_email = 'kquick@godaddy.com',
