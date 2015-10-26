2cf308800aa220d67f2b2c7dd9a27083b8621b0c
Kevin Quick <kquick@godaddy.com>
2015-10-26 16:05:22 -0700

    Thespian release 2.4.2 information

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'thespian',
-    version = '2.4.1',
+    version = '2.4.2',
     description = 'Python Actor concurrency library',
     author = 'Kevin Quick, GoDaddy',
     author_email = 'kquick@godaddy.com',
