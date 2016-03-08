003755ef3cc08f40f2b6737af8e93ded47f37c46
Kevin Quick <kquick@godaddy.com>
2016-03-08 11:23:56 -0700

    Update gen_pdf.sh to re-run pdflatex to fix cross references.

--- doc/gen_pdf.sh
+++ doc/gen_pdf.sh
@@ -1,2 +1,10 @@
 #emacs -batch --load doc/thespian.el --visit=$1 --funcall org-export-as-pdf
-emacs -batch --load doc/thespian.el --visit=$1 --funcall org-latex-export-to-pdf
+emacs -batch --load doc/thespian.el --visit=$1 --funcall org-latex-export-to-pdf 2>&1 | tee doc/pdf.log
+for x in 1 2 3 4 5 ; do
+    (cd doc;
+     texname=$(basename $1 .org).tex
+     if grep 'Rerun to get cross-references right' pdf.log ; then
+         pdflatex --interaction nonstopmode $texname | tee pdf.log
+     fi
+    )
+done
