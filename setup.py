f8a93bacc7d92ae66542c884c7a1d76122af4890
Kevin Quick <kquick@godaddy.com>
2016-03-14 08:28:23 -0700

    Thepsian 2.5.7 version updates.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'thespian',
-    version = '2.5.6',
+    version = '2.5.7',
     description = 'Python Actor concurrency library',
     author = 'Kevin Quick, GoDaddy',
     author_email = 'kquick@godaddy.com',
