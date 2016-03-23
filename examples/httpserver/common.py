1238cf39711229f75759ab5dfb990ca8f23f339f
Kevin Quick <kquick@godaddy.com>
2016-03-23 08:29:57 -0700

    Added http_server1 example

---
+++ examples/httpserver/common.py
@@ -0,0 +1,78 @@
+class HTTPRequest(object):
+    "Represents a complete HTTP client request"
+    def __init__(self, serveraddr, rmtaddr, req):
+        self.rmtaddr = rmtaddr
+        self._serveraddr = serveraddr
+        self.environ = self._parse_req(req)
+    def _parse_req(self, req):
+        reqlines = req.split(b'\r\n' if b'\r\n' in req else b'\n')
+        reqwords = reqlines[0].split(b' ')
+        urlparts = reqwords[1].split(b'?')
+        headers = map(lambda v: ((v if v[0] in [b'CONTENT_TYPE',
+                                                b'CONTENT_LENGTH'] else b'HTTP_'+ v[0]), v[1]),
+                      map(lambda w: ((w[0].strip().replace(b'-',b'_').upper()), w[2].strip()),
+                          map(lambda l: l.partition(b':'),
+                              reqlines[1:])))
+        return dict([ ('SERVER_NAME', self._serveraddr[0] or 'localhost'),
+                      ('SERVER_PORT', self._serveraddr[1]),
+                      ('REQUEST_METHOD', reqwords[0]),
+                      ('SERVER_PROTOCOL', reqwords[2]),
+                      ('PATH_INFO', urlparts[0]),
+                      ('QUERY_STRING', urlparts[1]),
+                      ('wsgi.version', (1, 0)),
+                      ('wsgi.url_scheme', 'http'),
+                      ('wsgi.input', ''),
+                      ('wsgi.errors', []),
+                      ('wsgi.multithread', False),
+                      ('wsgi.multiprocess', True),
+                      ('wsgi.run_once', False),
+        ] + list(headers))
+
+
+class HTTPRequestBuf(object):
+    """A buffer that accumulates data and can indicate when a complete
+       HTTP request is present in the data."""
+    def __init__(self, initial=None):
+        self.incbuf = initial or b''
+    def addMore(self, more):
+        self.incbuf += more
+    def __len__(self): return len(self.incbuf)
+    def isComplete(self):
+        return b'\r\n\r\n' in self.incbuf or b'\n\n' in self.incbuf
+    def extract(self, reqbuilder):
+        if self.isComplete():
+            endreq = b'\r\n\r\n' if b'\r\n\r\n' in self.incbuf else b'\n\n'
+            endloc = self.incbuf.index(endreq)
+            excess = self.incbuf[endloc + len(endreq):]
+            return reqbuilder(self.incbuf[:endloc]), HTTPRequestBuf(excess)
+        return None
+
+def parse_request(incsock, reqbuf):
+    return HTTPRequest(incsock.rmtaddr, reqbuf.incbuf)
+
+
+class IncomingSocket(object):
+    "Object for managing client sockets"
+    def __init__(self, acceptInfo):
+        self.socket, self.rmtaddr = acceptInfo
+        self.incbuf = HTTPRequestBuf()
+        self.expsize = None
+        self.socket.setblocking(0)
+
+
+class HTTPResponse(object):
+    def __init__(self, request, response_data, status=200, data_type='text/html'):
+        self.request = request
+        self.status = status
+        self.data = response_data
+        self.data_type = data_type
+    @property
+    def ststxt(self):
+        return {200: '200 OK',
+                }.get(self.status, '500 ERR')
+    def serialize(self):
+        return '\r\n'.join(['HTTP/1.1 ' + self.ststxt,
+                            'Content-Type: ' + self.data_type,
+                            'Content-Length: ' + str(len(self.data)),
+                            '',
+                            self.data]).encode('utf-8')
---
+++ examples/httpserver/http_server1.py
@@ -0,0 +1,167 @@
+"""Initial http server.
+
+  Wakes up each check period and accepts new incoming sockets and
+  services existing sockets.  All sockets are managed by the main
+  server actor.  When a complete HTTP request has been received, it is
+  sent to the Handler actor (provided at startup); the Handler actor
+  sends back a response which is then sent to the client.
+
+                 +---------+     +---------+
+   Clients------>| Server  |---->| Handler |
+                 +---------+     +---------+
+
+  Limitations:
+
+  * Because the socket is in a different Actor than the handler, the
+    handler cannot easily read incoming post data or stream large
+    output data.
+
+  * Only checks the listening socket once each check period, which
+    limit the number of client requests that can be handled.
+
+  * Only HTTP GET is supported, and the response is sent in a single
+    buffer (no streaming).
+
+  * Very basic
+
+"""
+
+import sys
+from thespian.actors import *
+import logging
+import select
+import socket
+import signal
+import errno
+from datetime import timedelta
+from functools import partial
+from common import *
+
+# n.b. this is a rather large value for demonstration purposes.  Lower
+# values give better responsiveness at the cost of idle overhead.
+CHECK_PERIOD = timedelta(seconds=3)
+
+
+class Handler(ActorTypeDispatcher):
+    def receiveMsg_HTTPRequest(self, reqmsg, sender):
+        # reqmsg.environ
+        self.send(sender, HTTPResponse(reqmsg, '<h1>Hello, World!</h1>'))
+
+
+class StartServer(object):
+    """Message sent to ServerActor to start HTTP server on a give port,
+       passing requests to the handler actor"""
+    def __init__(self, port, handler):
+        self.port = port
+        self.handler = handler
+
+
+class ServerActor(ActorTypeDispatcher):
+
+    def check_socket(self, incsock):
+        """Reads incoming data from a client-connected socket.  When a full
+           HTTP request has been received, sends it to the handler
+        """
+        try:
+            newdata = incsock.socket.recv((incsock.expsize or 65535) - len(incsock.incbuf))
+        except IOError as ex:
+            if ex.errno == errno.EAGAIN:
+                return True  # nothing to read right now
+            raise
+        if not newdata:
+            logging.info('Client socket from %s: closed', incsock.rmtaddr)
+            return False
+        logging.info('Client socket from %s: newdata -> %s', incsock.rmtaddr, newdata)
+        incsock.incbuf.addMore(newdata)
+        while incsock.incbuf.isComplete():
+            request, incsock.incbuf = incsock.incbuf.extract(partial(HTTPRequest, self.serveAddress, incsock.rmtaddr))
+            self.send(self.handler, request)
+        return True
+
+
+    def receiveMsg_HTTPResponse(self, msg, sender):
+        """Got an HTTP response from the handler; find the socket
+           corresponding to the request and send the response"""
+        for each in self.activesockets:
+            if each.rmtaddr == msg.request.rmtaddr:
+                each.socket.sendall(msg.serialize())
+                break
+        else:
+            logging.warning('Got HTTP response for %s but no socket to send', each.rmtaddr)
+
+
+    def check_incoming(self):
+        "Check the main listening socket for new connections"
+        logging.info('Check Incoming')
+        try:
+            while True:
+                newsock = self.servesocket.accept()
+                if not newsock: break
+                logging.info('Connect from %s', newsock[1])
+                self.activesockets.append(IncomingSocket(newsock))
+        except IOError:
+            pass
+
+
+    def receiveMsg_StartServer(self, m, sender):
+        """Creates the listening socket on the specified port and starts the
+           scheduled wakeups
+        """
+        logging.info('Starting server on %s', m.port)
+        self.handler = m.handler
+        self.serveAddress = ('', m.port)
+        if not getattr(self, 'servesocket', None):
+            self.servesocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
+            try:
+                self.servesocket.bind(self.serveAddress)
+                self.servesocket.listen(5)
+                self.servesocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+                self.servesocket.setblocking(0)
+            except Exception:
+                delattr(self, 'servesocket')
+            self.activesockets = []
+        self.wakeupAfter(CHECK_PERIOD)
+
+
+    def receiveMsg_WakeupMessage(self, m, sender):
+        "Checks for more stuff to handle on the sockets"
+        if hasattr(self, 'servesocket'):
+            try:
+                self.check_incoming()
+                self.activesockets = list(filter(self.check_socket, self.activesockets))
+            except Exception as e:
+                logging.critical("Got exception: %r", e)
+                self.send(self.myAddress, ActorExitRequest())
+                raise
+            self.wakeupAfter(CHECK_PERIOD)
+
+
+    def receiveMsg_ActorExitRequest(self, m, sender):
+        "Shut everyting down; actor is exiting"
+        if hasattr(self, 'servesocket'):
+            self.servesocket.close()
+            for each in self.activesockets:
+                each.socket.close()
+
+
+def main(portnum):
+    asys = ActorSystem('multiprocTCPBase')
+    try:
+        server = asys.createActor(ServerActor)
+        handler = asys.createActor(Handler)
+        asys.tell(server, StartServer(portnum, handler))
+        print('Hit Ctrl-C to exit')
+        try:
+            signal.pause()
+        except KeyboardInterrupt:
+            pass
+        print('Shutting down')
+        asys.tell(server, ActorExitRequest())
+        asys.tell(handler, ActorExitRequest())
+    finally:
+        asys.shutdown()
+
+
+
+if __name__ == '__main__':
+    main(int((sys.argv + [8080])[1]))
