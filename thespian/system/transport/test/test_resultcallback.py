59dc2602707190581b7943f50c29547af978115a
Kevin Quick <kquick@godaddy.com>
2016-07-04 22:40:59 -0700

    Update test_resultcallback to pytest.

--- thespian/system/transport/test/test_resultcallback.py
+++ thespian/system/transport/test/test_resultcallback.py
@@ -1,12 +1,9 @@
-import unittest
-import thespian.test.helpers
 from thespian.system.transport import ResultCallback
 from datetime import datetime, timedelta
 from time import sleep
 
 
-class TestResultCallback(unittest.TestCase):
-    scope="unit"
+class TestUnitResultCallback(object):
 
     def _good(self, result, value):
         if not hasattr(self, 'goods'): self.goods = []
@@ -21,60 +18,60 @@ class TestResultCallback(unittest.TestCase):
         self.fails = []
         rc = ResultCallback(self._good, self._fail)
         rc.resultCallback(True, 5)
-        self.assertEqual(self.goods, [(True, 5)])
-        self.assertEqual(self.fails, [])
+        assert self.goods == [(True, 5)]
+        assert self.fails == []
 
     def testFailCallback(self):
         self.goods = []
         self.fails = []
         rc = ResultCallback(self._good, self._fail)
         rc.resultCallback(False, 9)
-        self.assertEqual(self.goods, [])
-        self.assertEqual(self.fails, [(False, 9)])
+        assert self.goods == []
+        assert self.fails == [(False, 9)]
 
     def testGoodCallbackReCall(self):
         self.goods = []
         self.fails = []
         rc = ResultCallback(self._good, self._fail)
         rc.resultCallback(True, 5)
-        self.assertEqual(self.goods, [(True, 5)])
-        self.assertEqual(self.fails, [])
+        assert self.goods == [(True, 5)]
+        assert self.fails == []
         rc.resultCallback(True, 4)
-        self.assertEqual(self.goods, [(True, 5)])
-        self.assertEqual(self.fails, [])
+        assert self.goods == [(True, 5)]
+        assert self.fails == []
 
     def testFailCallbackReCall(self):
         self.goods = []
         self.fails = []
         rc = ResultCallback(self._good, self._fail)
         rc.resultCallback(False, 9)
-        self.assertEqual(self.goods, [])
-        self.assertEqual(self.fails, [(False, 9)])
+        assert self.goods == []
+        assert self.fails == [(False, 9)]
         rc.resultCallback(False, 8)
-        self.assertEqual(self.goods, [])
-        self.assertEqual(self.fails, [(False, 9)])
+        assert self.goods == []
+        assert self.fails == [(False, 9)]
 
     def testGoodCallbackReCallFail(self):
         self.goods = []
         self.fails = []
         rc = ResultCallback(self._good, self._fail)
         rc.resultCallback(True, 5)
-        self.assertEqual(self.goods, [(True, 5)])
-        self.assertEqual(self.fails, [])
+        assert self.goods == [(True, 5)]
+        assert self.fails == []
         rc.resultCallback(False, 4)
-        self.assertEqual(self.goods, [(True, 5)])
-        self.assertEqual(self.fails, [])
+        assert self.goods == [(True, 5)]
+        assert self.fails == []
 
     def testFailCallbackReCallGood(self):
         self.goods = []
         self.fails = []
         rc = ResultCallback(self._good, self._fail)
         rc.resultCallback(False, 9)
-        self.assertEqual(self.goods, [])
-        self.assertEqual(self.fails, [(False, 9)])
+        assert self.goods == []
+        assert self.fails == [(False, 9)]
         rc.resultCallback(True, 8)
-        self.assertEqual(self.goods, [])
-        self.assertEqual(self.fails, [(False, 9)])
+        assert self.goods == []
+        assert self.fails == [(False, 9)]
 
     def testManyGoodCallbacks(self):
         self.goods = []
@@ -82,8 +79,8 @@ class TestResultCallback(unittest.TestCase):
         rc = [ResultCallback(self._good, self._fail) for N in range(20)]
         for num,each in enumerate(rc):
             each.resultCallback(True, num)
-        self.assertEqual(self.goods, [(True, N) for N in range(20)])
-        self.assertEqual(self.fails, [])
+        assert self.goods == [(True, N) for N in range(20)]
+        assert self.fails == []
 
     def testManyFailCallbacks(self):
         self.goods = []
@@ -91,8 +88,8 @@ class TestResultCallback(unittest.TestCase):
         rc = [ResultCallback(self._good, self._fail) for N in range(20)]
         for num,each in enumerate(rc):
             each.resultCallback(False, num)
-        self.assertEqual(self.goods, [])
-        self.assertEqual(self.fails, [(False, N) for N in range(20)])
+        assert self.goods == []
+        assert self.fails == [(False, N) for N in range(20)]
 
     def testManyGoodAndFailCallbacks(self):
         self.goods = []
@@ -100,8 +97,8 @@ class TestResultCallback(unittest.TestCase):
         rc = [ResultCallback(self._good, self._fail) for N in range(20)]
         for num,each in enumerate(rc):
             each.resultCallback(0 == num % 3, num)
-        self.assertEqual(self.goods, [(True, N) for N in range(20) if N % 3 == 0])
-        self.assertEqual(self.fails, [(False, N) for N in range(20) if N % 3])
+        assert self.goods == [(True, N) for N in range(20) if N % 3 == 0]
+        assert self.fails == [(False, N) for N in range(20) if N % 3]
 
     def testChainedGoodCallbacks(self):
         self.goods = []
@@ -110,8 +107,8 @@ class TestResultCallback(unittest.TestCase):
         rc2 = ResultCallback(self._good, self._fail, rc)
         rc3 = ResultCallback(self._good, self._fail, rc2)
         rc3.resultCallback(True, 'good')
-        self.assertEqual(self.goods, [(True, 'good')] * 3)
-        self.assertEqual(self.fails, [])
+        assert self.goods == [(True, 'good')] * 3
+        assert self.fails == []
 
     def testChainedFailCallbacks(self):
         self.goods = []
@@ -120,8 +117,8 @@ class TestResultCallback(unittest.TestCase):
         rc2 = ResultCallback(self._good, self._fail, rc)
         rc3 = ResultCallback(self._good, self._fail, rc2)
         rc3.resultCallback(False, 'oops')
-        self.assertEqual(self.goods, [])
-        self.assertEqual(self.fails, [(False, 'oops')] * 3)
+        assert self.goods == []
+        assert self.fails == [(False, 'oops')] * 3
 
     def testChainedGoodCallbacksDoNotDuplicate(self):
         self.goods = []
@@ -130,11 +127,11 @@ class TestResultCallback(unittest.TestCase):
         rc2 = ResultCallback(self._good, self._fail, rc)
         rc3 = ResultCallback(self._good, self._fail, rc2)
         rc2.resultCallback(True, 'ok')
-        self.assertEqual(self.goods, [(True, 'ok'), (True, 'ok')])
-        self.assertEqual(self.fails, [])
+        assert self.goods == [(True, 'ok'), (True, 'ok')]
+        assert self.fails == []
         rc3.resultCallback(True, 'good')
-        self.assertEqual(self.goods, [(True, 'ok'), (True, 'ok'), (True, 'good')])
-        self.assertEqual(self.fails, [])
+        assert self.goods == [(True, 'ok'), (True, 'ok'), (True, 'good')]
+        assert self.fails == []
 
     def testChainedFailCallbacksDoNotDuplicate(self):
         self.goods = []
@@ -143,11 +140,11 @@ class TestResultCallback(unittest.TestCase):
         rc2 = ResultCallback(self._good, self._fail, rc)
         rc3 = ResultCallback(self._good, self._fail, rc2)
         rc2.resultCallback(False, 'bad')
-        self.assertEqual(self.goods, [])
-        self.assertEqual(self.fails, [(False, 'bad'), (False, 'bad')])
+        assert self.goods == []
+        assert self.fails == [(False, 'bad'), (False, 'bad')]
         rc3.resultCallback(False, 'oops')
-        self.assertEqual(self.goods, [])
-        self.assertEqual(self.fails, [(False, 'bad'), (False, 'bad'), (False, 'oops')])
+        assert self.goods == []
+        assert self.fails == [(False, 'bad'), (False, 'bad'), (False, 'oops')]
 
     def testChainedGoodCallbacksDoNotDuplicateOnFail(self):
         self.goods = []
@@ -156,11 +153,11 @@ class TestResultCallback(unittest.TestCase):
         rc2 = ResultCallback(self._good, self._fail, rc)
         rc3 = ResultCallback(self._good, self._fail, rc2)
         rc2.resultCallback(True, 'ok')
-        self.assertEqual(self.goods, [(True, 'ok'), (True, 'ok')])
-        self.assertEqual(self.fails, [])
+        assert self.goods == [(True, 'ok'), (True, 'ok')]
+        assert self.fails == []
         rc3.resultCallback(False, 'bad')
-        self.assertEqual(self.goods, [(True, 'ok'), (True, 'ok')])
-        self.assertEqual(self.fails, [(False, 'bad')])
+        assert self.goods == [(True, 'ok'), (True, 'ok')]
+        assert self.fails == [(False, 'bad')]
 
     def testChainedFailCallbacksDoNotDuplicateOnGood(self):
         self.goods = []
@@ -169,9 +166,9 @@ class TestResultCallback(unittest.TestCase):
         rc2 = ResultCallback(self._good, self._fail, rc)
         rc3 = ResultCallback(self._good, self._fail, rc2)
         rc2.resultCallback(False, 'bad')
-        self.assertEqual(self.goods, [])
-        self.assertEqual(self.fails, [(False, 'bad'), (False, 'bad')])
+        assert self.goods == []
+        assert self.fails == [(False, 'bad'), (False, 'bad')]
         rc3.resultCallback(True, 'yippee')
-        self.assertEqual(self.goods, [(True, 'yippee')])
-        self.assertEqual(self.fails, [(False, 'bad'), (False, 'bad')])
+        assert self.goods == [(True, 'yippee')]
+        assert self.fails == [(False, 'bad'), (False, 'bad')]
 
