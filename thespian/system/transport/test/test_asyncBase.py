6e8c66526d1afb40eac97149c2046ec448581833
Kevin Quick <kquick@godaddy.com>
2016-07-04 22:40:53 -0700

    Update test_asyncBase to pytest.

--- thespian/system/transport/test/test_asyncBase.py
+++ thespian/system/transport/test/test_asyncBase.py
@@ -2,7 +2,6 @@ from thespian.actors import ActorAddress
 from thespian.system.transport.asyncTransportBase import (asyncTransportBase,
                                                           MAX_PENDING_TRANSMITS)
 from thespian.system.transport import TransmitIntent, SendStatus
-import unittest
 
 
 class FakeTransport(asyncTransportBase):
@@ -26,11 +25,7 @@ class FakeTransport(asyncTransportBase):
                 return
 
 
-class TestAsyncTransportBase(unittest.TestCase):
-    scope='unit'
-
-    def setUp(self):
-        self.testTrans      = FakeTransport()
+class TestUnitAsyncTransportBase(object):
 
     def resetCounters(self):
         self.successCBcalls = 0
@@ -40,145 +35,161 @@ class TestAsyncTransportBase(unittest.TestCase):
     def failureCB(self, result, arg): self.failureCBcalls += 1
 
     def test_sendIntentToTransport(self):
+        testTrans = FakeTransport()
         testIntent = TransmitIntent(ActorAddress(None), 'message',
                                     self.successCB, self.failureCB)
-        self.testTrans.scheduleTransmit(None, testIntent)
-        self.assertEqual(1, len(self.testTrans.intents))
+        testTrans.scheduleTransmit(None, testIntent)
+        assert 1 == len(testTrans.intents)
 
     def test_sendIntentToTransportSuccessCallback(self):
+        testTrans = FakeTransport()
         self.resetCounters()
         testIntent = TransmitIntent(ActorAddress(0), 'message',
                                     self.successCB, self.failureCB)
-        self.testTrans.scheduleTransmit(None, testIntent)
-        self.assertEqual(1, len(self.testTrans.intents))
-        self.testTrans.forTestingCompleteAPendingIntent(SendStatus.Sent)
-        self.assertEqual(self.successCBcalls, 1)
-        self.assertEqual(self.failureCBcalls, 0)
+        testTrans.scheduleTransmit(None, testIntent)
+        assert 1 == len(testTrans.intents)
+        testTrans.forTestingCompleteAPendingIntent(SendStatus.Sent)
+        assert self.successCBcalls == 1
+        assert self.failureCBcalls == 0
 
     def test_sendIntentToTransportFailureCallback(self):
+        testTrans = FakeTransport()
         self.resetCounters()
         testIntent = TransmitIntent(ActorAddress(None), 'message',
                                     self.successCB, self.failureCB)
-        self.testTrans.scheduleTransmit(None, testIntent)
-        self.assertEqual(1, len(self.testTrans.intents))
-        self.testTrans.forTestingCompleteAPendingIntent(SendStatus.Failed)
-        self.assertEqual(self.successCBcalls, 0)
-        self.assertEqual(self.failureCBcalls, 1)
+        testTrans.scheduleTransmit(None, testIntent)
+        assert 1 == len(testTrans.intents)
+        testTrans.forTestingCompleteAPendingIntent(SendStatus.Failed)
+        assert self.successCBcalls == 0
+        assert self.failureCBcalls == 1
 
     extraTransmitIds = range(3000, 3003)
 
-    def test_sendIntentToTransportUpToLimitAndThenQueueInternally(self):
-
+    def _sendAndQueue(self, testTrans):
         # Initial transmits are all sent directly
         for count in range(MAX_PENDING_TRANSMITS):
-            self.testTrans.scheduleTransmit(
+            testTrans.scheduleTransmit(
                 None,
                 TransmitIntent(ActorAddress('me'), 'message%d'%count,
                                self.successCB, self.failureCB))
-        self.assertEqual(MAX_PENDING_TRANSMITS, len(self.testTrans.intents))
+        assert MAX_PENDING_TRANSMITS == len(testTrans.intents)
 
         # After that transmits are queued because none of the previous have completed
         for count in self.extraTransmitIds:
-            self.testTrans.scheduleTransmit(
+            testTrans.scheduleTransmit(
                 None,
                 TransmitIntent(ActorAddress(9), count,
                                self.successCB, self.failureCB))
-        self.assertEqual(MAX_PENDING_TRANSMITS, len(self.testTrans.intents))
-        self.assertFalse([I for I in self.testTrans.intents
-                          if I.message in self.extraTransmitIds])
+        assert MAX_PENDING_TRANSMITS == len(testTrans.intents)
+        assert not [I for I in testTrans.intents
+                    if I.message in self.extraTransmitIds]
+
+
+    def test_sendIntentToTransportUpToLimitAndThenQueueInternally(self):
+        testTrans = FakeTransport()
+        self._sendAndQueue(testTrans)
 
     def test_queueSendIntentIsSentOnSuccessCallbackOfPending(self):
+        testTrans = FakeTransport()
         self.resetCounters()
-        self.test_sendIntentToTransportUpToLimitAndThenQueueInternally()
-        self.testTrans.forTestingCompleteAPendingIntent(SendStatus.Sent)
-        self.assertEqual(self.successCBcalls, 1)
-        self.assertEqual(self.failureCBcalls, 0)
-        self.assertEqual(MAX_PENDING_TRANSMITS+1, len(self.testTrans.intents))
-        self.assertEqual(1, len([I for I in self.testTrans.intents
-                                 if I.message in self.extraTransmitIds]))
+        self._sendAndQueue(testTrans)
+        testTrans.forTestingCompleteAPendingIntent(SendStatus.Sent)
+        assert self.successCBcalls == 1
+        assert self.failureCBcalls == 0
+        assert MAX_PENDING_TRANSMITS+1 == len(testTrans.intents)
+        assert 1 == len([I for I in testTrans.intents
+                         if I.message in self.extraTransmitIds])
 
     def test_queueSendIntentIsSentOnFailureCallbackOfPending(self):
+        testTrans = FakeTransport()
         self.resetCounters()
-        self.test_sendIntentToTransportUpToLimitAndThenQueueInternally()
-        self.testTrans.forTestingCompleteAPendingIntent(SendStatus.Failed)
-        self.assertEqual(self.successCBcalls, 0)
-        self.assertEqual(self.failureCBcalls, 1)
-        self.assertEqual(MAX_PENDING_TRANSMITS+1, len(self.testTrans.intents))
-        self.assertEqual(1, len([I for I in self.testTrans.intents
-                                 if I.message in self.extraTransmitIds]))
+        self._sendAndQueue(testTrans)
+        testTrans.forTestingCompleteAPendingIntent(SendStatus.Failed)
+        assert self.successCBcalls == 0
+        assert self.failureCBcalls == 1
+        assert MAX_PENDING_TRANSMITS+1 == len(testTrans.intents)
+        assert 1 == len([I for I in testTrans.intents
+                         if I.message in self.extraTransmitIds])
 
     def test_allQueueSendIntentsAreSentOnEnoughPendingCallbackCompletions(self):
+        testTrans = FakeTransport()
         numExtras = len(self.extraTransmitIds)
         self.resetCounters()
-        self.test_sendIntentToTransportUpToLimitAndThenQueueInternally()
-        self.assertTrue(MAX_PENDING_TRANSMITS > numExtras)
+        self._sendAndQueue(testTrans)
+        assert MAX_PENDING_TRANSMITS > numExtras
         for _extras in range(numExtras):
-            self.testTrans.forTestingCompleteAPendingIntent(SendStatus.Sent)
-        self.assertEqual(self.successCBcalls, numExtras)
-        self.assertEqual(MAX_PENDING_TRANSMITS + numExtras, len(self.testTrans.intents))
-        self.assertEqual(numExtras, len([I for I in self.testTrans.intents
-                                         if I.message in self.extraTransmitIds]))
+            testTrans.forTestingCompleteAPendingIntent(SendStatus.Sent)
+        assert self.successCBcalls == numExtras
+        assert MAX_PENDING_TRANSMITS + numExtras == len(testTrans.intents)
+        assert numExtras == len([I for I in testTrans.intents
+                                 if I.message in self.extraTransmitIds])
 
     def test_extraPendingCallbackCompletionsDoNothing(self):
+        self._extraCompletions(FakeTransport())
+
+    def _extraCompletions(self, testTrans):
         numExtras = len(self.extraTransmitIds)
         self.resetCounters()
-        self.test_sendIntentToTransportUpToLimitAndThenQueueInternally()
-        self.assertTrue(MAX_PENDING_TRANSMITS > numExtras + 3)
+        self._sendAndQueue(testTrans)
+        assert MAX_PENDING_TRANSMITS > numExtras + 3
         for _extras in range(numExtras + 3):
-            self.testTrans.forTestingCompleteAPendingIntent(SendStatus.Sent)
-        self.assertEqual(self.successCBcalls, numExtras + 3)
-        self.assertEqual(MAX_PENDING_TRANSMITS + numExtras, len(self.testTrans.intents))
-        self.assertEqual(numExtras, len([I for I in self.testTrans.intents
-                                         if I.message in self.extraTransmitIds]))
+            testTrans.forTestingCompleteAPendingIntent(SendStatus.Sent)
+        assert self.successCBcalls == numExtras + 3
+        assert MAX_PENDING_TRANSMITS + numExtras == len(testTrans.intents)
+        assert numExtras == len([I for I in testTrans.intents
+                                 if I.message in self.extraTransmitIds])
 
     def test_pendingCallbacksClearQueueAndMoreRunsRunAdditionalQueuedOnMoreCompletions(self):
+        testTrans = FakeTransport()
         numExtras = len(self.extraTransmitIds)
-        self.test_extraPendingCallbackCompletionsDoNothing()
+        self._extraCompletions(testTrans)
         # Add more transmits to reach MAX_PENDING_TRANSMITS again
         for _moreExtras in range(numExtras + 3):
-            self.testTrans.forTestingCompleteAPendingIntent(SendStatus.Sent)
+            testTrans.forTestingCompleteAPendingIntent(SendStatus.Sent)
         expectedCBCount = 2 * (numExtras + 3)
-        self.assertEqual(self.successCBcalls, expectedCBCount)
-        self.assertEqual(MAX_PENDING_TRANSMITS + numExtras, len(self.testTrans.intents))
-        self.assertEqual(numExtras, len([I for I in self.testTrans.intents
-                                         if I.message in self.extraTransmitIds]))
+        assert self.successCBcalls == expectedCBCount
+        assert MAX_PENDING_TRANSMITS + numExtras == len(testTrans.intents)
+        assert numExtras == len([I for I in testTrans.intents
+                                 if I.message in self.extraTransmitIds])
         # Now send more and make sure they are queued
         for count in self.extraTransmitIds:
-            self.testTrans.scheduleTransmit(
+            testTrans.scheduleTransmit(
                 None,
                 TransmitIntent(ActorAddress(3.5), count,
                                self.successCB, self.failureCB))
-        self.assertEqual(self.successCBcalls, expectedCBCount)
-        self.assertEqual(MAX_PENDING_TRANSMITS + 2 * numExtras, len(self.testTrans.intents))
-        self.assertEqual(2 * numExtras, len([I for I in self.testTrans.intents
-                                             if I.message in self.extraTransmitIds]))
+        assert self.successCBcalls == expectedCBCount
+        assert MAX_PENDING_TRANSMITS + 2 * numExtras == len(testTrans.intents)
+        assert 2 * numExtras == len([I for I in testTrans.intents
+                                     if I.message in self.extraTransmitIds])
         # And verify that more completions cause the newly Queued to be run
         for _extras in range(numExtras):
-            self.testTrans.forTestingCompleteAPendingIntent(SendStatus.Sent)
+            testTrans.forTestingCompleteAPendingIntent(SendStatus.Sent)
         expectedCBCount += numExtras
-        self.assertEqual(self.successCBcalls, expectedCBCount)
-        self.assertEqual(MAX_PENDING_TRANSMITS + numExtras + numExtras,
-                         len(self.testTrans.intents))
-        self.assertEqual(2 * numExtras, len([I for I in self.testTrans.intents
-                                             if I.message in self.extraTransmitIds]))
+        assert self.successCBcalls == expectedCBCount
+        assert (MAX_PENDING_TRANSMITS +
+                numExtras +
+                numExtras) == len(testTrans.intents)
+        assert 2 * numExtras == len([I for I in testTrans.intents
+                                     if I.message in self.extraTransmitIds])
 
     def test_transmitsCompletingWithCallbackDoNotQueue(self):
+        testTrans = FakeTransport()
         self.resetCounters()
         for count in range(MAX_PENDING_TRANSMITS):
-            self.testTrans.scheduleTransmit(
+            testTrans.scheduleTransmit(
                 None,
                 TransmitIntent(ActorAddress(self), 'message%d'%count,
                                self.successCB, self.failureCB))
-            self.testTrans.forTestingCompleteAPendingIntent(SendStatus.Sent)
-        self.assertEqual(self.successCBcalls, MAX_PENDING_TRANSMITS)
+            testTrans.forTestingCompleteAPendingIntent(SendStatus.Sent)
+        assert self.successCBcalls == MAX_PENDING_TRANSMITS
 
         numExtras = len(self.extraTransmitIds)
         for count in self.extraTransmitIds:
-            self.testTrans.scheduleTransmit(
+            testTrans.scheduleTransmit(
                 None,
-                TransmitIntent(ActorAddress(self.testTrans), count,
+                TransmitIntent(ActorAddress(testTrans), count,
                                self.successCB, self.failureCB))
-            self.testTrans.forTestingCompleteAPendingIntent(SendStatus.Sent)
-        self.assertEqual(MAX_PENDING_TRANSMITS + numExtras, len(self.testTrans.intents))
-        self.assertEqual(numExtras, len([I for I in self.testTrans.intents
-                                         if I.message in self.extraTransmitIds]))
+            testTrans.forTestingCompleteAPendingIntent(SendStatus.Sent)
+        assert MAX_PENDING_TRANSMITS + numExtras == len(testTrans.intents)
+        assert numExtras == len([I for I in testTrans.intents
+                                 if I.message in self.extraTransmitIds])
