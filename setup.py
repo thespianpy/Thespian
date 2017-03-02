f3409f83fcd785a6180ee2918200688e39c0fbf4
Kevin Quick <kquick@godaddy.com>
2017-03-02 13:27:34 -0700

    Thespian 3.6.3 release updates.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'thespian',
-    version = '3.6.2',
+    version = '3.6.3',
     description = 'Python Actor concurrency library',
     author = 'Kevin Quick, GoDaddy',
     author_email = 'kquick@godaddy.com',
