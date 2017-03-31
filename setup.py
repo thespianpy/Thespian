2f4bffdeec04d43dcf2a1c448b83e9f39ab58f68
Kevin Quick <quick@sparq.org>
2017-03-30 23:20:19 -0700

    Thespian 3.7.2 release updates.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'thespian',
-    version = '3.7.1',
+    version = '3.7.2',
     description = 'Python Actor concurrency library',
     author = 'Kevin Quick',
     author_email = 'quick@sparq.org',
