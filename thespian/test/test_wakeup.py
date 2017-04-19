6de1dae9337c585e909f486aa410a8fae7e9e7b4
Kevin Quick <quick@sparq.org>
2017-04-18 22:09:36 -0700

    Adjust internal sleep time for wakeup tests and Multiprocessing Queue base.

--- thespian/test/test_wakeup.py
+++ thespian/test/test_wakeup.py
@@ -15,10 +15,14 @@ from thespian.actors import *
 # Note that this test is highly subjective based on the scheduling
 # sequence, message delivery latency, and responsiveness of the
 # current system.  Significantly larger numbers here will increase the
-# stability of the test at the expense of test duration.
+# stability of the test at the expense of test duration.  The
+# Multiprocess Queue library seems to have longer internal delays.
 wakeupAfterPeriod = timedelta(seconds=0.065)
-sleepLongerThanWakeup = lambda: time.sleep(0.6)
+sleepLongerThanWakeup = lambda sys: time.sleep(2.1
+                                               if sys.base_name == 'multiprocQueueBase'
+                                               else 0.6)
 sleepPartOfWakeupPeriod = lambda: time.sleep(0.005)
+
 
 
 class RetryActor(Actor):
@@ -45,7 +49,7 @@ class TestFuncWakeup(object):
         # to run after the previous statement.
         assert asys.ask(waiter, 'awoken?', 1) == 0
 
-        sleepLongerThanWakeup()
+        sleepLongerThanWakeup(asys)
 
         assert asys.ask(waiter, 'awoken?', 1) == 1
 
@@ -59,18 +63,18 @@ class TestFuncWakeup(object):
         # to run after the previous statement.
         assert asys.ask(waiter, 'awoken?', 1) == 0
 
-        sleepLongerThanWakeup()
+        sleepLongerThanWakeup(asys)
         assert asys.ask(waiter, 'awoken?', 1) == 1
 
         asys.tell(waiter, 'check')
         # ditto above
         assert asys.ask(waiter, 'awoken?', 1) == 1
 
-        sleepLongerThanWakeup()
+        sleepLongerThanWakeup(asys)
         asys.tell(waiter, 'check')
         assert asys.ask(waiter, 'awoken?', 1) == 2
 
-        sleepLongerThanWakeup()
+        sleepLongerThanWakeup(asys)
         assert asys.ask(waiter, 'awoken?', 1) == 3
 
 
@@ -84,7 +88,7 @@ class TestFuncWakeup(object):
         # to run after the previous statement.
         assert asys.ask(waiter, 'awoken?', 1) == 0
 
-        sleepLongerThanWakeup()
+        sleepLongerThanWakeup(asys)
         assert asys.ask(waiter, 'awoken?', 1) == 2
 
         asys.tell(waiter, 'check')
@@ -93,8 +97,9 @@ class TestFuncWakeup(object):
         sleepPartOfWakeupPeriod()
         asys.tell(waiter, 'check')
 
-        sleepLongerThanWakeup()
-        assert asys.ask(waiter, 'awoken?', 1) == 5
+        sleepLongerThanWakeup(asys)
+        awoken = asys.ask(waiter, 'awoken?', 1)
+        assert awoken == 5
 
 
     def test_exitWithWakeupsPending(self, asys):
