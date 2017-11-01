22d5cfbd6cf880ce13de599a07af97c1290f33f7
Kevin Quick <quick@sparq.org>
2017-11-01 10:53:05 -0700

    Remove declaration of support for Python 2.6

--- setup.py
+++ setup.py
@@ -16,7 +16,6 @@ setup(
         'Operating System :: MacOS',
         'Operating System :: Microsoft :: Windows',
         'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
