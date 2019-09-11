155dc2e14269509c72953f87441606286002d355
Kevin Quick <kquick@galois.com>
2019-09-10 23:13:39 -0700

    Thespian release 3.9.11 updates.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'thespian',
-    version = '3.9.10',
+    version = '3.9.11',
     description = 'Python Actor concurrency library',
     author = 'Kevin Quick',
     author_email = 'quick@sparq.org',
