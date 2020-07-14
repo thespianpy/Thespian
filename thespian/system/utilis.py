2e8e60b1f5577d36859eaed43435106d3eca2456
Kevin Quick <kquick@galois.com>
2020-07-14 00:41:03 -0700

    Fix initial logging level from THESPLOG_THRESHOLD for undefined

--- thespian/system/utilis.py
+++ thespian/system/utilis.py
@@ -22,7 +22,7 @@ _name_to_level = {
 
 # Default/current logging controls
 _thesplog_control_settings = (
-    _name_to_level.get(os.getenv('THESPLOG_THRESHOLD'), logging.WARNING),
+    _name_to_level.get(os.getenv('THESPLOG_THRESHOLD') or 'WARNING', logging.WARNING),
     False,
     os.getenv('THESPLOG_FILE_MAXSIZE', 50 * 1024) # 50KB by default
 )
